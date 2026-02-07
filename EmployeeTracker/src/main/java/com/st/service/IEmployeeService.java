0ed5ed2ac81b9fb0abe47942bd3009325a278f6a
Shantanu M Ghate <146326185+Shantanughate@users.noreply.github.com>
2026-02-07 11:13:02 +0530

    Create IEmployeeService.java

---
+++ EmployeeTracker/src/main/java/com/st/service/IEmployeeService.java
@@ -0,0 +1,17 @@
+package com.st.service;
+
+import com.main.entity.Employee;
+import com.main.exception.EmployeeNotFoundException;
+
+public interface IEmployeeService {
+
+	public Iterable<Employee> getAllEmployee();
+	
+	public String registerEmployee(Employee emp);
+	
+	public Employee getEmployeeByEmpNo(Integer empno) throws EmployeeNotFoundException;
+	
+	public String updateEmployee(Employee emp);
+	
+	public String deleteEmployeeByEmpno(Integer empno);
+}
