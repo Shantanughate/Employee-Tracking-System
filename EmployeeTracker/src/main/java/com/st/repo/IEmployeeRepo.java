50c6bef22fbb6fd41be30b0aba5b52854bfbf257
Shantanu M Ghate <146326185+Shantanughate@users.noreply.github.com>
2026-02-07 11:06:29 +0530

    Update IEmployeeRepo.java

--- EmployeeTracker/src/main/java/com/st/repo/IEmployeeRepo.java
+++ EmployeeTracker/src/main/java/com/st/repo/IEmployeeRepo.java
@@ -1,1 +1,11 @@
+package com.st.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.main.entity.Employee;
+
+public interface IEmployeeRepo extends CrudRepository<Employee,Integer> {
+
+	
+}
 
