7ac2ca6ffcc9cdc071958a84f580034b1bba5cd8
Shantanu M Ghate <146326185+Shantanughate@users.noreply.github.com>
2026-02-07 11:42:18 +0530

    Update import statement for Employee entity

--- EmployeeTracker/src/main/java/com/st/repo/IEmployeeRepo.java
+++ EmployeeTracker/src/main/java/com/st/repo/IEmployeeRepo.java
@@ -2,7 +2,7 @@ package com.st.repo;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.main.entity.Employee;
+import com.st.entity.Employee;
 
 public interface IEmployeeRepo extends CrudRepository<Employee,Integer> {
 
