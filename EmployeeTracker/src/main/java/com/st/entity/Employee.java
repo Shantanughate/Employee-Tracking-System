fdc7c63c4aa95b19261f42c75924a40efcbc37d4
Shantanu M Ghate <146326185+Shantanughate@users.noreply.github.com>
2026-02-07 11:01:55 +0530

    Enhance Employee class with JPA and Lombok annotations
    Added JPA annotations and Lombok to Employee class.

--- EmployeeTracker/src/main/java/com/st/entity/Employee.java
+++ EmployeeTracker/src/main/java/com/st/entity/Employee.java
@@ -1,1 +1,31 @@
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.Data;
+
+@Entity
+@Table(name="Employee")
+@Data
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer empno;
+	
+	@Column(length=20)
+	private String ename;
+	
+	@Column(length=20)
+	private String job;
+	
+//	@Transient this annotation is used for the field that we don't want to be persisted in dbtable and whose value we don't want to show to client
+	private Double salary;
+	
+	private Integer deptno;
+	
+}
